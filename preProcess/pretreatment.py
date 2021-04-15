85129ea807a27d9efa6c8f0f37a2db7610e265a1
mapletea <peggy471665@gmail.com>
2021-04-15 15:24:52 +0800

    image_to_pixel

---
+++ preProcess/pretreatment.py
@@ -0,0 +1,64 @@
+import os
+import csv
+import numpy as np
+from PIL import Image
+from keras.preprocessing.image import img_to_array
+from numpy import expand_dims
+
+def img_to_pixel(fl, size, gray):
+    #  --- transform image to array and normalize it ---  #
+    # @fl: the image file
+    # @size: the size of image array
+    # @gray: use gray or not
+    
+    if gray:
+        image = Image.open(fl).convert('L')
+    else:
+        image = Image.open(fl)
+        
+    image = image.resize((size,size))
+    data = np.array(image)
+    data = data.reshape(size*size)
+    data = data.astype('uint8')
+    return data 
+    
+    
+def make_data(image_dir, save_dir, size, gray):
+    #  --- make image data to array and save csv ---  #
+    # @image_dir: the image dir 
+    # @save_dir: the array data save dir
+    # @size: the image size wanted to transform
+    # @gray: use gray or not
+
+    label_dict={
+        'science': 0, # 科院
+        'manage': 1, # 管院
+        'edu': 2, # 教院
+        'human': 3, # 人院
+        'admin': 4, # 行政大樓
+        'library': 5, # 圖書館
+        'activity': 6, # 學活
+        'restaurant': 7, # 學餐
+        'restrauant': 7, # 學餐
+    }
+
+    if gray:
+        save_filename = save_dir+'gray_data_'+str(size)+'.csv'
+    else:
+        save_filename = save_dir+'RGB_data_'+str(size)+'.csv'
+    with open(save_filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        pixel_list = []
+        for i in range(size*size):
+            pixel_list.append("pixel"+str(i))
+        writer.writerow(['label']+pixel_list)
+        for path in image_dir:
+            files = os.listdir(path)
+            for fl in files:
+                pixels = img_to_pixel(path+fl, size, gray)
+                label = label_dict[fl.split('_')[0]]
+                writer.writerow([str(label)]+pixels.tolist())
+    csvfile.close()
+
+if __name__ == '__main__':
+    make_data(image_dir=['../data/label_building/', '../data/augmentation/'], save_dir='../data/array/', size = 128, gray=False)
